503214973c84f852d64840064b23a5c5646a5460
smturek <smturek@gmail.com>
2015-09-30 08:44:11 -0600

    read terrains from json file

--- js/prefabs/Board.js
+++ js/prefabs/Board.js
@@ -9,6 +9,14 @@ HexGame.Board = function(state, grid) {
     this.rows = this.grid.length;
     this.cols = this.grid[0].length;
 
+    this.terrains = [
+        {asset: 'grass'},
+        {asset: 'water', blocked: true},
+        {asset: 'rocks'},
+        {asset: 'grasstrees'},
+        {asset: 'grasstrees2'}
+    ];
+
     //create hexagons
     var row, col, title, x, y;
     for(row = 0; row < this.rows; row++) {
@@ -24,8 +32,19 @@ HexGame.Board = function(state, grid) {
 
             y = this.state.MARGIN_Y + row * this.state.TILE_H * 3/4;
 
-            tile = new Phaser.Sprite(this.game, x, y, 'grass');
+            //create new tile
+            tile = new Phaser.Sprite(this.game, x, y, this.terrains[this.grid[row][col]].asset);
 
+            //keep some information in the tile object
+            tile.row = row;
+            tile.col = col;
+            tile.terrainAsset = this.terrains[this.grid[row][col]].asset;
+            tile.blocked = this.terrains[this.grid[row][col]].blocked;
+
+            tile.inputEnabled = true;
+            tile.input.pixelPerfectClick = true;
+
+            //add tile to Board group
             this.add(tile);
         }
     }
