4e8a67091fa42284de4b4bd39800133462f92c2e
smturek <smturek@gmail.com>
2015-10-02 10:18:49 -0600

    added movement options

--- js/prefabs/Unit.js
+++ js/prefabs/Unit.js
@@ -10,10 +10,34 @@ HexGame.Unit = function(state, data) {
     this.board = state.board;
     this.row = data.row;
     this.col = data.col;
-    this.data = data
+    this.data = data;
 
     this.anchor.setTo(0.5);
 };
 
 HexGame.Unit.prototype = Object.create(Phaser.Sprite.prototype);
 HexGame.Unit.prototype.constructor = HexGame.Unit;
+
+HexGame.Unit.prototype.showMovementOptions = function() {
+    //only will happen if the UI is free
+    if(this.state.uiBlocked) {
+        return;
+    }
+
+    //get current tile
+    var currTile = this.board.getFromRowCol(this.row, this.col);
+
+    //get adjacent tiles
+    var adjacentTiles = this.board.getAdjacent(currTile, true);
+
+    adjacentTiles.forEach(function(tile) {
+        tile.alpha = 0.7;
+
+        //add imput
+        tile.events.onInputDown.add(this.moveUnit, this);
+    }, this);
+};
+
+HexGame.Unit.prototype.moveUnit = function() {
+    console.log("move unit");
+}
